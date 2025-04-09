0997c7809bde2539f0549a2239366a00f7b7fc49
Riikiki <lambertnameonyou@gmail.com>
2025-04-09 15:30:26 +0200

    creating models v1 with no migrations

--- raising/elevage/models.py
+++ raising/elevage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class rabbit(models.Model):
+    genre = models.IntegerField()
+    
+class cage(models.Model):
+    nbRabbit = models.ForeignKey(rabbit, on_delete=models.CASCADE)
+    
+class cash(models.Model):
+    funds = models.FloatField(default=0.0)
+    
+class elevage(models.Model):
+    nbCage = models.ForeignKey(cage, on_delete=models.CASCADE)
+    
+class player(models.Model):
+    username = models.CharField(max_length=100, null=True, blank=True)
+    password = models.CharField(max_length=100, null=True, blank=True)
+    inGameRaising = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.username
